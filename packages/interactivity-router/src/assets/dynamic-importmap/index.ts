3448e0f39c3763e95001ced73e6078a5924c5f5b
David <david.arenas@automattic.com>
2025-02-06 14:02:17 +0100

    Improve types and TSDocs a little bit more

--- packages/interactivity-router/src/assets/dynamic-importmap/index.ts
+++ packages/interactivity-router/src/assets/dynamic-importmap/index.ts
@@ -4,6 +4,11 @@
 import { addImportMap, resolve } from './resolver';
 import { initPromise, registry, topLevelLoad, preloadModule } from './loader';
 
+type ImportMap = {
+	imports?: Record< string, string >;
+	scopes?: Record< string, Record< string, string > >;
+};
+
 // TODO: check if this baseURI should change per document, and so
 // it need to be passed as a parameter to methods like `importWithMap`
 // and `preloadWithMap`.
@@ -15,19 +20,42 @@ const pageBaseUrl = baseUrl;
 ( self as any ).importShim = importShim;
 importShim._r = registry;
 
-async function importShim( id ) {
+async function importShim< Module = unknown >( id: string ) {
 	await initPromise;
-	return topLevelLoad( ( await resolve( id, pageBaseUrl ) ).r, {
+	return topLevelLoad< Module >( ( await resolve( id, pageBaseUrl ) ).r, {
 		credentials: 'same-origin',
 	} );
 }
 
-export async function importWithMap( id, importMapIn ) {
+/**
+ * Import the module with the passed ID.
+ *
+ * The module is resolved against the internal dynamic import map,
+ * extended with the passed import map.
+ *
+ * @param id          Module ID.
+ * @param importMapIn Import map.
+ * @return Resolved module.
+ */
+export async function importWithMap< Module = unknown >(
+	id: string,
+	importMapIn: ImportMap
+) {
 	addImportMap( importMapIn );
-	return importShim( id );
+	return importShim< Module >( id );
 }
 
-export async function preloadWithMap( id, importMapIn ) {
+/**
+ * Preload the module with the passed ID along with its dependencies.
+ *
+ * The module is resolved against the internal dynamic import map,
+ * extended with the passed import map.
+ *
+ * @param id          Module ID.
+ * @param importMapIn Import map.
+ * @return Resolved `ModuleLoad` instance.
+ */
+export async function preloadWithMap( id: string, importMapIn: ImportMap ) {
 	addImportMap( importMapIn );
 	await initPromise;
 	return preloadModule( ( await resolve( id, pageBaseUrl ) ).r, {
