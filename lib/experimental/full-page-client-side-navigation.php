19e6373b96e66ee50a557871ad08c95f73b9ab67
David <david.arenas@automattic.com>
2025-04-09 19:56:42 +0200

    Refactor logic to add directives in BODY

--- lib/experimental/full-page-client-side-navigation.php
+++ lib/experimental/full-page-client-side-navigation.php
@@ -33,69 +33,41 @@ function _gutenberg_add_enhanced_pagination_to_query_block( $parsed_block ) {
 add_filter( 'render_block_data', '_gutenberg_add_enhanced_pagination_to_query_block' );
 
 /**
- * Adds client-side navigation directives to BODY tag.
+ * Adds client-side navigation directives to BODY tag in the passed output
+ * buffer.
  *
- * Note: This should probably be done per site, not by default when this option is enabled.
+ * Note: This should probably be done per site, not by default when this option
+ * is enabled.
  *
- * @param string $response_body The response body.
+ * @param string $buffer Passed output buffer.
  *
- * @return string The rendered template with modified BODY attributes.
+ * @return string The same HTML with modified BODY attributes.
  */
-function _gutenberg_add_client_side_navigation_directives( $response_body ) {
-	$is_html_content_type = false;
-	foreach ( headers_list() as $header ) {
-		$header_parts = preg_split( '/\s*[:;]\s*/', strtolower( $header ) );
-		if ( count( $header_parts ) >= 2 && 'content-type' === $header_parts[0] ) {
-			$is_html_content_type = in_array( $header_parts[1], array( 'text/html', 'application/xhtml+xml' ), true );
-		}
-	}
-	if ( ! $is_html_content_type ) {
-		return $response_body;
-	}
-
-	$p = new WP_HTML_Tag_Processor( $response_body );
+function _gutenberg_add_client_side_navigation_directives( $buffer ) {
+	$p = new WP_HTML_Tag_Processor( $buffer );
 	if ( $p->next_tag( array( 'tag_name' => 'BODY' ) ) ) {
-		$p->set_attribute( 'data-wp-interactive', 'core/experimental' );
-		$p->set_attribute( 'data-wp-context', '{}' );
-		$response_body = $p->get_updated_html();
+		$p->set_attribute( 'data-wp-interactive', true );
+		$p->set_attribute( 'data-wp-router-region', 'body' );
+		return $p->get_updated_html();
+	} else {
+		return $buffer;
 	}
-	return $response_body;
 }
-
-// TODO: Explore moving this to the server directive processing.
-add_filter( 'gutenberg_template_output_buffer', '_gutenberg_add_client_side_navigation_directives' );
 
 /**
- * Starts output buffering at the end of the 'template_include' filter.
- *
- * This is to implement #43258 in core.
- *
- * This is a hack which would eventually be replaced with something like this in wp-includes/template-loader.php:
- *
- *          $template = apply_filters( 'template_include', $template );
- *     +    ob_start( 'wp_template_output_buffer_callback' );
- *          if ( $template ) {
- *              include $template;
- *          } elseif ( current_user_can( 'switch_themes' ) ) {
- *
- * @link https://core.trac.wordpress.org/ticket/43258
- *
- * @param string $passthrough Value for the template_include filter which is passed through.
- *
- * @return string Unmodified value of $passthrough.
+ * Starts output buffering at the end of the 'wp_head' action, adding the
+ * required directives for client-side navigation to the BODY tags when the
+ * buffer is flushed.
  */
-function _gutenberg_buffer_template_output( string $passthrough ): string {
-	ob_start(
-		static function ( string $output ): string {
-			/**
-			 * Filters the template output buffer prior to sending to the client.
-			 *
-			 * @param string $output Output buffer.
-			 * @return string Filtered output buffer.
-			 */
-			return (string) apply_filters( 'gutenberg_template_output_buffer', $output );
-		}
-	);
-	return $passthrough;
+function _gutenberg_full_page_client_navigation_buffer_start() {
+	ob_start( '_gutenberg_add_client_side_navigation_directives' );
 }
-add_filter( 'template_include', '_gutenberg_buffer_template_output', PHP_INT_MAX );
+add_action( 'wp_head', '_gutenberg_full_page_client_navigation_buffer_start' );
+
+/**
+ * Flushes the output buffer at the end of the 'wp_footer' action.
+ */
+function _gutenberg_full_page_client_navigation_buffer_end() {
+	ob_end_flush();
+}
+add_action( 'wp_footer', '_gutenberg_full_page_client_navigation_buffer_end' );
