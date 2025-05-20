1ddb035614f9163fe83b9b24699e183f11e94b0f
David <david.arenas@automattic.com>
2025-05-20 17:15:14 +0200

    Move lighbox fix to Full_Page_Navigation class

--- lib/experimental/interactivity-api/class-wp-interactivity-api-full-page-navigation.php
+++ lib/experimental/interactivity-api/class-wp-interactivity-api-full-page-navigation.php
@@ -28,6 +28,8 @@ if ( ! class_exists( 'WP_Interactivity_API_Full_Page_Navigation' ) ) {
 			add_action( 'init', array( $this, 'set_default_mode' ), 9 );
 			add_action( 'wp_head', array( $this, 'buffer_start' ) );
 			add_action( 'wp_footer', array( $this, 'buffer_end' ), 8 );
+			add_action( 'wp_footer', array( $this, 'core_image_lightbox_buffer_start' ), 9 );
+			add_action( 'wp_footer', array( $this, 'core_image_lightbox_buffer_end' ), 11 );
 			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_script_modules' ) );
 		}
 
@@ -103,6 +105,43 @@ if ( ! class_exists( 'WP_Interactivity_API_Full_Page_Navigation' ) ) {
 			} else {
 				return $buffer;
 			}
+		}
+
+		/**
+		 * Modifies the lightbox element injected by the `core/image` block.
+		 *
+		 * This function appends new directives to make this element compatible with
+		 * the full-page client-side navigation feature.
+		 *
+		 * @param string $buffer Passed output buffer.
+		 *
+		 * @return string Same HTML with new directives.
+		 */
+		public function add_lightbox_region_directives( $buffer ) {
+			$p = new WP_HTML_Tag_Processor( $buffer );
+			if ( $p->next_tag( array( 'class_name' => 'wp-lightbox-overlay' ) ) ) {
+				$p->set_attribute( 'data-wp-router-region', '{ "id": "core/body", "attachTo": "body" }' );
+				$p->set_attribute( 'data-wp-key', 'wp-lightbox-overlay' );
+				$p->set_attribute( 'data-wp-class--show-closing-animation', 'state.overlayOpened' );
+				return $p->get_updated_html();
+			} else {
+				return $buffer;
+			}
+		}
+
+		/**
+		 * Starts buffering the output to capture the lightbox element markup.
+		 */
+		public function core_image_lightbox_buffer_start() {
+			ob_start( array( $this, 'add_lightbox_region_directives' ) );
+		}
+
+		/**
+		 * Stops and flushes the captured lightbox element markup with new
+		 * directives for compatibility with the full-page navigation.
+		 */
+		public function core_image_lightbox_buffer_end() {
+			ob_end_flush();
 		}
 	}
 }
